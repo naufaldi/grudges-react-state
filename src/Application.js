9a44c7dabe6ee55f5fe54a2c20f4b47e2b84d596
Naufaldi Rafif <naufaldirafif@Faiks-MacBook-Air.local>
2020-07-16 17:10:30 +0700

    add forgive

--- src/Application.js
+++ src/Application.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useCallback } from 'react';
 
 import id from 'uuid/v4';
 
@@ -9,10 +9,16 @@ import initialState from './initialState';
 
 const GRUDGE_ADD = 'GRUDGE_ADD';
 const GRUDGE_FORGIVE = 'GRUDGE_FORGIVE';
-
+//react memo, if has same props. dont render it
 const reducer = (state, action) => {
   if (action.type === GRUDGE_ADD) {
     return [action.payload, ...state];
+  }
+  if(action.type === GRUDGE_FORGIVE){
+    return state.map(grudge => {
+          if (grudge.id !== action.payload.id ) return grudge;
+          return { ...grudge, forgiven: !grudge.forgiven };
+        })
   }
 
   return state;
@@ -21,7 +27,7 @@ const reducer = (state, action) => {
 const Application = () => {
   const [grudges, dispatch] = useReducer(reducer, initialState);
 
-  const addGrudge = ({ person, reason }) => {
+  const addGrudge = useCallback(({ person, reason }) => {
     dispatch({
       type: GRUDGE_ADD,
       payload: {
@@ -31,16 +37,14 @@ const Application = () => {
         id: id()
       }
     });
-  };
+  },[dispatch]);
 
-  const toggleForgiveness = id => {
-    // setGrudges(
-    //   grudges.map(grudge => {
-    //     if (grudge.id !== id) return grudge;
-    //     return { ...grudge, forgiven: !grudge.forgiven };
-    //   })
-    // );
-  };
+  const toggleForgiveness = useCallback(id => {
+    dispatch({
+      type: GRUDGE_FORGIVE,
+      payload :{ id}
+    })
+  },[dispatch]);
 
   return (
     <div className="Application">
