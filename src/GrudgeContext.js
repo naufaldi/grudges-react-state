d13641e3ab0034e5c7e567394a8f5f8cb13eedc5
Naufaldi Rafif <naufaldirafif@Faiks-MacBook-Air.local>
2020-07-28 23:30:45 +0700

    dont understand

--- src/GrudgeContext.js
+++ src/GrudgeContext.js
@@ -7,31 +7,46 @@ export const GrudgeContext = createContext();
 const GRUDGE_ADD = 'GRUDGE_ADD';
 const GRUDGE_FORGIVE = 'GRUDGE_FORGIVE';
 
-const reducer = (state = [], action) => {
+const reducer = (state = defaultState, action) => {
   if (action.type === GRUDGE_ADD) {
-    return [
+    const newPresent = [
       {
         id: id(),
         ...action.payload
       },
-      ...state
+      ...state.present
     ];
+    return {
+      past: [state.present, ...state.past],
+      present: newPresent,
+      future: []
+    }
   }
 
   if (action.type === GRUDGE_FORGIVE) {
-    return state.map(grudge => {
-      if (grudge.id === action.payload.id) {
-        return { ...grudge, forgiven: !grudge.forgiven };
+    const newPresent = state.present.ap(grudge => {
+      if( grudge.id === action.payload.id ){
+        return { ...grudge, forgiven : !grudge.forgiven};
       }
       return grudge;
-    });
+    })
+    return {
+      past: [],
+      present: newPresent,
+      future: []
+    }; 
   }
-
+ 
   return state;
 };
-
+const defaultState = {
+  past: [],
+  present: initialState,
+  future: []
+}
 export const GrudgeProvider = ({ children }) => {
   const [grudges, dispatch] = useReducer(reducer, initialState);
+  const grudges =  state.present;
 
   const addGrudge = useCallback(
     ({ person, reason }) => {
